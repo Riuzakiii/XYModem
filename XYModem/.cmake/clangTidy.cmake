e85ceccc755bcde36dffe03c659030584af58900
Xav83 <x.jouvenot@gmail.com>
2022-05-21 19:04:05 +0200

    Adds the clang-tidy check: modernize-avoid-bind.

--- XYModem/.cmake/clangTidy.cmake
+++ XYModem/.cmake/clangTidy.cmake
@@ -8,5 +8,6 @@ find_program (
     REQUIRED
    )
 
-set(CLANG_TIDY_CHECKS bugprone-*,clang-analyzer-*,concurrency-*,performance-*,portability-*)
+set(CLANG_TIDY_MODERNIZE_CHECKS modernize-avoid-bind)
+set(CLANG_TIDY_CHECKS bugprone-*,clang-analyzer-*,concurrency-*,${CLANG_TIDY_MODERNIZE_CHECKS},performance-*,portability-*)
 set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} --checks=${CLANG_TIDY_CHECKS} --warnings-as-errors=${CLANG_TIDY_CHECKS})
