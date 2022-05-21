3300a39d0c6ef054b18191bcba5cecf6939368e0
Xav83 <x.jouvenot@gmail.com>
2022-05-21 19:48:28 +0200

    Adds the clang-tidy check: modernize-deprecated-headers.

--- XYModem/.cmake/clangTidy.cmake
+++ XYModem/.cmake/clangTidy.cmake
@@ -8,6 +8,6 @@ find_program (
     REQUIRED
    )
 
-set(CLANG_TIDY_MODERNIZE_CHECKS modernize-avoid-bind,modernize-concat-nested-namespaces)
+set(CLANG_TIDY_MODERNIZE_CHECKS modernize-avoid-bind,modernize-concat-nested-namespaces,modernize-deprecated-headers)
 set(CLANG_TIDY_CHECKS bugprone-*,clang-analyzer-*,concurrency-*,${CLANG_TIDY_MODERNIZE_CHECKS},performance-*,portability-*)
 set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} --checks=${CLANG_TIDY_CHECKS} --warnings-as-errors=${CLANG_TIDY_CHECKS})
