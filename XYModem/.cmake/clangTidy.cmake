ef71535384aa3f9c5786d4c2630ac839f1a7acf5
Xav83 <x.jouvenot@gmail.com>
2022-05-21 20:10:47 +0200

    Adds the clang-tidy check: modernize-pass-by-value.

--- XYModem/.cmake/clangTidy.cmake
+++ XYModem/.cmake/clangTidy.cmake
@@ -8,6 +8,6 @@ find_program (
     REQUIRED
    )
 
-set(CLANG_TIDY_MODERNIZE_CHECKS modernize-avoid-bind,modernize-concat-nested-namespaces,modernize-deprecated-headers,modernize-deprecated-ios-base-aliases,modernize-macro-to-enum,modernize-make-shared,modernize-make-unique)
+set(CLANG_TIDY_MODERNIZE_CHECKS modernize-avoid-bind,modernize-concat-nested-namespaces,modernize-deprecated-headers,modernize-deprecated-ios-base-aliases,modernize-macro-to-enum,modernize-make-shared,modernize-make-unique,modernize-pass-by-value)
 set(CLANG_TIDY_CHECKS bugprone-*,clang-analyzer-*,concurrency-*,${CLANG_TIDY_MODERNIZE_CHECKS},performance-*,portability-*)
 set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} --checks=${CLANG_TIDY_CHECKS} --warnings-as-errors=${CLANG_TIDY_CHECKS})
