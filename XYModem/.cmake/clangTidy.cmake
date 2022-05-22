96669ebc1344a46020b86e05d66da4398ef64aa0
Xav83 <x.jouvenot@gmail.com>
2022-05-22 13:44:15 +0200

    Adds the clang-tidy check: cppcoreguidelines-narrowing-conversions.

--- XYModem/.cmake/clangTidy.cmake
+++ XYModem/.cmake/clangTidy.cmake
@@ -8,7 +8,7 @@ find_program (
     REQUIRED
    )
 
-set(CLANG_TIDY_CHECKS_CPPCORE_GUIDELINE cppcoreguidelines-avoid-goto,cppcoreguidelines-avoid-non-const-global-variables,cppcoreguidelines-init-variables,cppcoreguidelines-interfaces-global-init,cppcoreguidelines-macro-usage)
-set(CLANG_TIDY_MODERNIZE_CHECKS modernize-*,-modernize-avoid-c-arrays,-modernize-use-trailing-return-type)
+   set(CLANG_TIDY_CHECKS_CPPCORE_GUIDELINE cppcoreguidelines-avoid-goto,cppcoreguidelines-avoid-non-const-global-variables,cppcoreguidelines-init-variables,cppcoreguidelines-interfaces-global-init,cppcoreguidelines-macro-usage,cppcoreguidelines-narrowing-conversions)
+   set(CLANG_TIDY_MODERNIZE_CHECKS modernize-*,-modernize-avoid-c-arrays,-modernize-use-trailing-return-type)
 set(CLANG_TIDY_CHECKS bugprone-*,clang-analyzer-*,concurrency-*,${CLANG_TIDY_CHECKS_CPPCORE_GUIDELINE},${CLANG_TIDY_MODERNIZE_CHECKS},performance-*,portability-*)
 set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} --checks=${CLANG_TIDY_CHECKS} --warnings-as-errors=${CLANG_TIDY_CHECKS})
