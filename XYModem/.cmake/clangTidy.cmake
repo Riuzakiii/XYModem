a9eae9b4395bf375323307e692e5527baa61e0f8
Xav83 <x.jouvenot@gmail.com>
2022-05-22 13:32:48 +0200

    Adds the clang-tidy check: cppcoreguidelines-init-variables.

--- XYModem/.cmake/clangTidy.cmake
+++ XYModem/.cmake/clangTidy.cmake
@@ -8,7 +8,7 @@ find_program (
     REQUIRED
    )
 
-set(CLANG_TIDY_CHECKS_CPPCORE_GUIDELINE cppcoreguidelines-avoid-goto,cppcoreguidelines-avoid-non-const-global-variables)
+set(CLANG_TIDY_CHECKS_CPPCORE_GUIDELINE cppcoreguidelines-avoid-goto,cppcoreguidelines-avoid-non-const-global-variables,cppcoreguidelines-init-variables)
 set(CLANG_TIDY_MODERNIZE_CHECKS modernize-*,-modernize-avoid-c-arrays,-modernize-use-trailing-return-type)
 set(CLANG_TIDY_CHECKS bugprone-*,clang-analyzer-*,concurrency-*,${CLANG_TIDY_CHECKS_CPPCORE_GUIDELINE},${CLANG_TIDY_MODERNIZE_CHECKS},performance-*,portability-*)
 set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} --checks=${CLANG_TIDY_CHECKS} --warnings-as-errors=${CLANG_TIDY_CHECKS})
