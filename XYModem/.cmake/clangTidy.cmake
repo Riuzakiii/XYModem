af4974122cbf807d1504a0f0521e8ee83afe4451
Xav83 <x.jouvenot@gmail.com>
2022-05-23 13:31:31 +0200

    Adds the clang-tidy check: misc-definitions-in-headers.

--- XYModem/.cmake/clangTidy.cmake
+++ XYModem/.cmake/clangTidy.cmake
@@ -8,8 +8,9 @@ find_program (
     REQUIRED
    )
 
+set(CLANG_TIDY_CHECKS_MISC misc-definitions-in-headers)
 set(CLANG_TIDY_CHECKS_READABILITY readability-*,-readability-magic-numbers)
 set(CLANG_TIDY_CHECKS_CPPCORE_GUIDELINE cppcoreguidelines-*,-cppcoreguidelines-avoid-magic-numbers,-cppcoreguidelines-avoid-c-arrays)
 set(CLANG_TIDY_MODERNIZE_CHECKS modernize-*,-modernize-avoid-c-arrays,-modernize-use-trailing-return-type)
-set(CLANG_TIDY_CHECKS bugprone-*,clang-analyzer-*,concurrency-*,${CLANG_TIDY_CHECKS_CPPCORE_GUIDELINE},${CLANG_TIDY_MODERNIZE_CHECKS},performance-*,portability-*,${CLANG_TIDY_CHECKS_READABILITY})
+set(CLANG_TIDY_CHECKS bugprone-*,clang-analyzer-*,concurrency-*,${CLANG_TIDY_CHECKS_CPPCORE_GUIDELINE},${CLANG_TIDY_CHECKS_MISC},${CLANG_TIDY_MODERNIZE_CHECKS},performance-*,portability-*,${CLANG_TIDY_CHECKS_READABILITY})
 set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} --checks=${CLANG_TIDY_CHECKS} --warnings-as-errors=${CLANG_TIDY_CHECKS})
