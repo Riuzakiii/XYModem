d2d2a6285d169b834225b6934246dc60c411c8d3
Xav83 <x.jouvenot@gmail.com>
2022-05-31 23:19:30 +0200

    Set up a basic integration of cppcheck in the build system

---
+++ XYModem/.cmake/cppcheck.cmake
@@ -0,0 +1,11 @@
+
+find_program (
+    CPPCHECK_EXE
+    NAMES cppcheck cppcheck.exe
+    HINTS "/usr/bin"
+    DOC "cppcheck executable"
+    NO_CACHE
+    REQUIRED
+   )
+
+set(CMAKE_CXX_CPPCHECK ${CPPCHECK_EXE} --std=c++${CMAKE_CXX_STANDARD} --library=/usr/lib/x86_64-linux-gnu/cppcheck/cfg/googletest.cfg --enable=warning)
