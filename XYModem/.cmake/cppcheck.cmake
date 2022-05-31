abaf6d47ea3c3e3e2b127bb43b475f7a4b8e0678
Xav83 <x.jouvenot@gmail.com>
2022-05-31 23:33:46 +0200

    Adds the cppcheck style warnings to the static analysis

--- XYModem/.cmake/cppcheck.cmake
+++ XYModem/.cmake/cppcheck.cmake
@@ -8,4 +8,4 @@ find_program (
     REQUIRED
    )
 
-set(CMAKE_CXX_CPPCHECK ${CPPCHECK_EXE} --std=c++${CMAKE_CXX_STANDARD} --library=/usr/lib/x86_64-linux-gnu/cppcheck/cfg/googletest.cfg --enable=warning)
+set(CMAKE_CXX_CPPCHECK ${CPPCHECK_EXE} --std=c++${CMAKE_CXX_STANDARD} --library=/usr/lib/x86_64-linux-gnu/cppcheck/cfg/googletest.cfg --enable=warning,style)
